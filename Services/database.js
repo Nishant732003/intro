c842fa74a389e8d97b6795f02e2990d8f4235e08
nishant <mahtonishantraj@gmail.com>
2024-05-28 02:38:07 +0530

    final commit

--- Services/database.js
+++ Services/database.js
@@ -2,6 +2,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString:
-    "postgresql://Users_owner:phcxa2uSFkb0@ep-wandering-base-a1cbz947-pooler.ap-southeast-1.aws.neon.tech/Assignment?sslmode=require"
+    POSTGRES_URI
 });
 module.exports = { pool };
